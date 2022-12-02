fd87f8af4ce70f56637ae07d21c57d31d9a5a634
Aleksandra Czaban <o.czaban@gmail.com>
2022-12-02 15:58:28 +0100

    zmiany

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -40,4 +40,22 @@ class TicTacToe:
         self.board[row][col] = player
 
     def is_player_win(self, player):
+        if self.board[0][0] == player and self.board[0][1] == player and self.board[0][2] == player:
+            return True
+        if self.board[1][0] == player and self.board[1][1] == player and self.board[1][2] == player:
+            return True
+        if self.board[2][0] == player and self.board[2][1] == player and self.board[2][2] == player:
+            return True
+        if self.board[0][0] == player and self.board[1][0] == player and self.board[2][0] == player:
+            return True
+        if self.board[0][1] == player and self.board[1][1] == player and self.board[2][1] == player:
+            return True
+        if self.board[0][2] == player and self.board[1][2] == player and self.board[2][2] == player:
+            return True
+        if self.board[0][0] == player and self.board[1][1] == player and self.board[2][2] == player:
+            return True
+        if self.board[2][0] == player and self.board[1][1] == player and self.board[0][2] == player:
+            return True
+
         return False
+
