4920114db961d07ccbba02542bbc86416e9da9f6
Paweł Halicki <halicki@gmail.com>
2022-12-03 20:21:29 +0100

    Zadanie 2: FizzBuzz game

---
+++ FizzBuzz/__Init__.py
@@ -0,0 +1,5 @@
+from FizzBuzz.fizzBuzz import fizzBuzz
+
+for i in range(1,101):
+    print( str(i) + ": " + str(fizzBuzz(i)) )
+
---
+++ FizzBuzz/fizzBuzz.py
@@ -0,0 +1,11 @@
+'''
+Funkcja przyjmuje liczbe naturalna większą bądz równą 1
+
+dla zadanej liczby sprawdza wszystkie warunki
+- jezeli jest podzielna przez 3 to komunikuje to dodajac Fizz w odpowiedzi
+- jezeli jest podzielna przez 5 to komunikuje to dodajac Bazz w odpowiedzi
+- jeżeli wczesniejszy warunek nie zostal spelniony, zwracana jest w odpowiedzi zadana liczba
+'''
+
+def fizzBuzz(num):
+    return str(num)
