8f269f878306cfbf94da940b8bdf3578515b28fd
ACzaban <106841213+ACzaban@users.noreply.github.com>
2023-01-24 23:15:16 +0100

    Add files via upload

---
+++ pliki/pliki cwiczenia.py
@@ -0,0 +1,30 @@
+#ćwiczenie 1 7.12
+fhand = open(input('Podaj nazwę: '))
+x = fhand.read()
+print(x.upper())
+
+
+
+#ćwiczenie 2 7.12 + ćwiczenie 3
+fname = input('Podaj nazwę: ')
+if fname == 'trele morele':
+    print('co za bzdury')
+    exit()
+try:
+    fhand = open(fname)
+except:
+    print('nie można otworzyc pliku')
+    exit()
+zlicz = 0
+total = 0
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('X-DSPAM-Confidence:'):
+        continue
+    start = line.find(': ')
+    liczba = float(line[start+1:])
+    zlicz = zlicz + 1
+
+    total = total + liczba
+    srednia = total / zlicz
+print('średni poziom pewności spamu: ', srednia)
