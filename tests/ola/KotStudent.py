64b6371eca95cffefdd0b1e2e01dc859a6dd68f3
Aleksandra Czaban <o.czaban@gmail.com>
2023-01-31 00:15:01 +0100

    KotStudent cd2

--- tests/ola/KotStudent.py
+++ tests/ola/KotStudent.py
@@ -2,68 +2,79 @@ import math
 from unittest import TestCase
 import math
 
+
 class KotStudent:
-    def __init__(self, r, h):
-        self.promien = r
-        self.wysokosc = h
 
-
-    def poleKola(self):
-        '''pole = math.pi * (self.promien * self.promien)'''
-        pole = math.pi * pow(self.promien, 2)
+    def poleKola(self, promien):
+        pole = math.pi * pow(promien, 2)
         return round(pole, 2)
 
-    def objetoscWalca(self):
-        objetoscw = self.poleKola() * self.wysokosc
+    def objetoscWalca(self, promien, wysokosc):
+        objetoscw = self.poleKola(promien) * wysokosc
         return round(objetoscw, 2)
 
-    def objetoscStozka(self):
-        objetoscs = 1/3 * self.objetoscWalca()
+    def objetoscStozka(self, promien, wysokosc):
+        objetoscs = 1 / 3 * self.objetoscWalca(promien, wysokosc)
         return round(objetoscs, 2)
-
-
 
 
 class Test(TestCase):
     def test_kotstudent1(self):
-        kot = KotStudent(1)
+        kot = KotStudent()
 
-        self.assertEqual(kot.poleKola(), 3.14)
+        self.assertEqual(kot.poleKola(1), 3.14)
 
     def test_kotstudent2(self):
-        kot = KotStudent(3)
+        kot = KotStudent()
 
-        self.assertEqual(kot.poleKola(), 28.27)
+        self.assertEqual(kot.poleKola(3), 28.27)
 
     def test_kotstudent3(self):
-        kot = KotStudent(5)
+        kot = KotStudent()
 
-        self.assertEqual(kot.poleKola(), 78.54)
+        self.assertEqual(kot.poleKola(5), 78.54)
 
     def test_kotstudent4(self):
-        kot = KotStudent(11)
+        kot = KotStudent()
 
-        self.assertEqual(kot.poleKola(), 380.13)
+        self.assertEqual(kot.poleKola(11), 380.13)
 
     def test_kotstudent5(self):
-        kot = KotStudent(42)
+        kot = KotStudent()
 
-        self.assertEqual(kot.poleKola(), 5541.77)
+        self.assertEqual(kot.poleKola(42), 5541.77)
 
     def test_kotstudent6(self):
-        kot = KotStudent(101)
+        kot = KotStudent()
 
-        self.assertEqual(kot.poleKola(), 32047.39)
+        self.assertEqual(kot.poleKola(101), 32047.39)
 
-    def test_kotstudent7walec(self):
-        kot = KotStudent(3, 3)
+    def test_kotstudentwalec1(self):
+        kot = KotStudent()
 
-        self.assertEqual(kot.objetoscWalca(), 84.81)
+        self.assertEqual(kot.objetoscWalca(3, 3), 84.81)
 
-    def test_kotstudent8stozek(self):
-        kot = KotStudent(5, 5)
+    def test_kotstudentwalec2(self):
+        kot = KotStudent()
 
-        self.assertEqual(kot.objetoscStozka(), 130.9)
+        self.assertEqual(kot.objetoscWalca(5, 5), 392.7)
 
+    def test_kotstudentwalec3(self):
+        kot = KotStudent()
 
+        self.assertEqual(kot.objetoscWalca(11, 11), 4181.43)
 
+    def test_kotstudentstozek1(self):
+        kot = KotStudent()
+
+        self.assertEqual(kot.objetoscStozka(3, 3), 28.27)
+
+    def test_kotstudentstozek2(self):
+        kot = KotStudent()
+
+        self.assertEqual(kot.objetoscStozka(5, 5), 130.9)
+
+    def test_kotstudentstozek3(self):
+        kot = KotStudent()
+
+        self.assertEqual(kot.objetoscStozka(11, 11), 1393.81)
