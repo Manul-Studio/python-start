00c9bf2d9d59c6ca8620aeef6e284316bad63a01
Aleksandra Czaban <o.czaban@gmail.com>
2023-02-20 23:29:53 +0100

    regexy poprawki, rip x.x

--- pliki/regex cwiczenia.py
+++ pliki/regex cwiczenia.py
@@ -5,8 +5,7 @@ hand = open('mbox.txt')
 regex = input('Podaj wyrazenie regularne: ')
 count = 0
 for line in hand:
-    line = line.rstrip()
-    if re.findall(regex, line):
+    if re.search(regex, line):
         count = count+1
 
 print('mbox.txt ma', count, 'linii pasujących do', regex)
@@ -14,14 +13,7 @@ print('mbox.txt ma', count, 'linii pasujących do', regex)
 #CW2
 name = input('Podaj nazwę pliku: ')
 hand = open(name)
-count = 0
-sum = 0
-lst = list()
-for line in hand:
-    line = line.rstrip()
-    x = re.findall('^New.*: ([0-9]+)', line)
-    for numbers in x:
-        numbers = int(numbers)
-        sum = sum + numbers
-        count = count + 1
-print(int(sum/count))
+inp = hand.read()
+x = re.findall('New Revision: ([0-9]+)', inp)
+lst = list(map(int, x))
+print(int(sum(lst)/len(lst)))
